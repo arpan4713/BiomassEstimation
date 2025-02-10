7b66cef5b19c9699af7746ce035d66fa35133599
arpan4713 <arpan.linkd@gmail.com>
2025-02-10 23:57:53 +0530

    modified

--- architectures.py
+++ architectures.py
@@ -122,6 +122,109 @@ above is the original
 '''
 eurosat cp
 '''
+# import torch
+# from torch import nn
+# from torch.nn import functional as F
+# from torch.autograd import Variable
+# import numpy as np
+
+# class FC_Encoder(nn.Module):
+#     def __init__(self, output_size, input_size=784):
+#         super(FC_Encoder, self).__init__()
+#         self.fc1 = nn.Linear(input_size, output_size)
+
+#     def forward(self, x):
+#         h1 = F.relu(self.fc1(x))
+#         return h1
+
+# class FC_Decoder(nn.Module):
+#     def __init__(self, embedding_size, output_size=784):
+#         super(FC_Decoder, self).__init__()
+#         self.fc3 = nn.Linear(embedding_size, 1024)
+#         self.fc4 = nn.Linear(1024, output_size)
+
+#     def forward(self, z):
+#         h3 = F.relu(self.fc3(z))
+#         return torch.sigmoid(self.fc4(h3))
+
+# class CNN_Encoder(nn.Module):
+#     def __init__(self, output_size, input_size=(3, 64, 64)):
+#         super(CNN_Encoder, self).__init__()
+#         self.input_size = input_size
+#         self.channel_mult = 16
+#         in_channels = input_size[0]
+
+#         self.conv = nn.Sequential(
+#             nn.Conv2d(in_channels, self.channel_mult, 4, 2, 1),
+#             nn.LeakyReLU(0.2, inplace=True),
+#             nn.Conv2d(self.channel_mult, self.channel_mult*2, 4, 2, 1),
+#             nn.BatchNorm2d(self.channel_mult*2),
+#             nn.LeakyReLU(0.2, inplace=True),
+#             nn.Conv2d(self.channel_mult*2, self.channel_mult*4, 4, 2, 1),
+#             nn.BatchNorm2d(self.channel_mult*4),
+#             nn.LeakyReLU(0.2, inplace=True),
+#             nn.Conv2d(self.channel_mult*4, self.channel_mult*8, 4, 2, 1),
+#             nn.BatchNorm2d(self.channel_mult*8),
+#             nn.LeakyReLU(0.2, inplace=True)
+#         )
+
+#         self.flat_fts = self.get_flat_fts(self.conv)
+#         self.linear = nn.Sequential(
+#             nn.Linear(self.flat_fts, output_size),
+#             nn.BatchNorm1d(output_size),
+#             nn.LeakyReLU(0.2)
+#         )
+
+#     def get_flat_fts(self, fts):
+#         f = fts(Variable(torch.ones(1, *self.input_size)))
+#         return int(np.prod(f.size()[1:]))
+
+#     def forward(self, x):
+#         x = self.conv(x)
+#         x = x.view(-1, self.flat_fts)
+#         return self.linear(x)
+
+# class CNN_Decoder(nn.Module):
+#     def __init__(self, embedding_size, output_size=(3, 64, 64)):
+#         super(CNN_Decoder, self).__init__()
+#         self.output_size = output_size
+#         self.channel_mult = 16
+#         out_channels = output_size[0]
+
+#         self.fc = nn.Sequential(
+#             nn.Linear(embedding_size, 512),
+#             nn.BatchNorm1d(512),
+#             nn.ReLU(True)
+#         )
+
+#         self.deconv = nn.Sequential(
+#             nn.ConvTranspose2d(512, self.channel_mult*8, 4, 1, 0, bias=False),
+#             nn.BatchNorm2d(self.channel_mult*8),
+#             nn.ReLU(True),
+#             nn.ConvTranspose2d(self.channel_mult*8, self.channel_mult*4, 4, 2, 1, bias=False),
+#             nn.BatchNorm2d(self.channel_mult*4),
+#             nn.ReLU(True),
+#             nn.ConvTranspose2d(self.channel_mult*4, self.channel_mult*2, 4, 2, 1, bias=False),
+#             nn.BatchNorm2d(self.channel_mult*2),
+#             nn.ReLU(True),
+#             nn.ConvTranspose2d(self.channel_mult*2, self.channel_mult, 4, 2, 1, bias=False),
+#             nn.BatchNorm2d(self.channel_mult),
+#             nn.ReLU(True),
+#             nn.ConvTranspose2d(self.channel_mult, out_channels, 4, 2, 1, bias=False),
+#             nn.Sigmoid()
+#         )
+
+#     def forward(self, x):
+#         x = self.fc(x)
+#         x = x.view(-1, 512, 1, 1)
+#         x = self.deconv(x)
+#         return x
+    
+
+'''
+      eurosat ds
+'''
+
 import torch
 from torch import nn
 from torch.nn import functional as F
@@ -129,26 +232,30 @@ from torch.autograd import Variable
 import numpy as np
 
 class FC_Encoder(nn.Module):
-    def __init__(self, output_size, input_size=784):
+    def __init__(self, latent_dim, input_size=784):
         super(FC_Encoder, self).__init__()
-        self.fc1 = nn.Linear(input_size, output_size)
+        self.fc1 = nn.Linear(input_size, 512)
+        self.fc_mu = nn.Linear(512, latent_dim)  # Mean of the latent space
+        self.fc_logvar = nn.Linear(512, latent_dim)  # Log variance of the latent space
 
     def forward(self, x):
         h1 = F.relu(self.fc1(x))
-        return h1
+        mu = self.fc_mu(h1)  # Mean
+        logvar = self.fc_logvar(h1)  # Log variance
+        return mu, logvar
 
 class FC_Decoder(nn.Module):
-    def __init__(self, embedding_size, output_size=784):
+    def __init__(self, latent_dim, output_size=784):
         super(FC_Decoder, self).__init__()
-        self.fc3 = nn.Linear(embedding_size, 1024)
-        self.fc4 = nn.Linear(1024, output_size)
+        self.fc2 = nn.Linear(latent_dim, 512)
+        self.fc3 = nn.Linear(512, output_size)
 
     def forward(self, z):
-        h3 = F.relu(self.fc3(z))
-        return torch.sigmoid(self.fc4(h3))
+        h2 = F.relu(self.fc2(z))
+        return torch.sigmoid(self.fc3(h2))  # Output in range [0, 1]
 
 class CNN_Encoder(nn.Module):
-    def __init__(self, output_size, input_size=(3, 64, 64)):
+    def __init__(self, latent_dim, input_size=(3, 64, 64)):
         super(CNN_Encoder, self).__init__()
         self.input_size = input_size
         self.channel_mult = 16
@@ -169,11 +276,8 @@ class CNN_Encoder(nn.Module):
         )
 
         self.flat_fts = self.get_flat_fts(self.conv)
-        self.linear = nn.Sequential(
-            nn.Linear(self.flat_fts, output_size),
-            nn.BatchNorm1d(output_size),
-            nn.LeakyReLU(0.2)
-        )
+        self.fc_mu = nn.Linear(self.flat_fts, latent_dim)  # Mean of the latent space
+        self.fc_logvar = nn.Linear(self.flat_fts, latent_dim)  # Log variance of the latent space
 
     def get_flat_fts(self, fts):
         f = fts(Variable(torch.ones(1, *self.input_size)))
@@ -182,17 +286,19 @@ class CNN_Encoder(nn.Module):
     def forward(self, x):
         x = self.conv(x)
         x = x.view(-1, self.flat_fts)
-        return self.linear(x)
+        mu = self.fc_mu(x)  # Mean
+        logvar = self.fc_logvar(x)  # Log variance
+        return mu, logvar
 
 class CNN_Decoder(nn.Module):
-    def __init__(self, embedding_size, output_size=(3, 64, 64)):
+    def __init__(self, latent_dim, output_size=(3, 64, 64)):
         super(CNN_Decoder, self).__init__()
         self.output_size = output_size
         self.channel_mult = 16
         out_channels = output_size[0]
 
         self.fc = nn.Sequential(
-            nn.Linear(embedding_size, 512),
+            nn.Linear(latent_dim, 512),
             nn.BatchNorm1d(512),
             nn.ReLU(True)
         )
@@ -211,7 +317,7 @@ class CNN_Decoder(nn.Module):
             nn.BatchNorm2d(self.channel_mult),
             nn.ReLU(True),
             nn.ConvTranspose2d(self.channel_mult, out_channels, 4, 2, 1, bias=False),
-            nn.Sigmoid()
+            nn.Sigmoid()  # Output in range [0, 1]
         )
 
     def forward(self, x):
--- models/VAE.py
+++ models/VAE.py
@@ -110,6 +110,93 @@ original aboveee
 cp eurosat
 '''
 
+# import torch
+# import torch.utils.data
+# from torch import nn, optim
+# from torch.nn import functional as F
+
+# import sys
+# sys.path.append('../')
+# from architectures import FC_Encoder, FC_Decoder, CNN_Encoder, CNN_Decoder
+# from datasets import get_dataloader
+
+# class Network(nn.Module):
+#     def __init__(self, args):
+#         super(Network, self).__init__()
+#         output_size = 512
+#         self.encoder = CNN_Encoder(output_size)
+#         self.var = nn.Linear(output_size, args.embedding_size)
+#         self.mu = nn.Linear(output_size, args.embedding_size)
+#         self.decoder = CNN_Decoder(args.embedding_size)
+
+#     def encode(self, x):
+#         x = self.encoder(x)
+#         return self.mu(x), self.var(x)
+
+#     def reparameterize(self, mu, logvar):
+#         std = torch.exp(0.5 * logvar)
+#         eps = torch.randn_like(std)
+#         return eps.mul(std).add_(mu)
+
+#     def decode(self, z):
+#         return self.decoder(z)
+
+#     def forward(self, x):
+#         mu, logvar = self.encode(x)
+#         z = self.reparameterize(mu, logvar)
+#         return self.decode(z), mu, logvar
+
+# class VAE(object):
+#     def __init__(self, args):
+#         self.args = args
+#         self.device = torch.device("cuda" if args.cuda else "cpu")
+#         self.train_loader = get_dataloader(self.args.dataset, self.args.batch_size)
+
+#         self.model = Network(args).to(self.device)
+#         self.optimizer = optim.Adam(self.model.parameters(), lr=1e-3)
+
+#     def loss_function(self, recon_x, x, mu, logvar):
+#         BCE = F.binary_cross_entropy(recon_x, x, reduction='sum')
+#         KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+#         return BCE + KLD
+
+#     def train(self, epoch):
+#         self.model.train()
+#         train_loss = 0
+#         for batch_idx, (data, _) in enumerate(self.train_loader):
+#             data = data.to(self.device)
+#             self.optimizer.zero_grad()
+#             recon_batch, mu, logvar = self.model(data)
+#             loss = self.loss_function(recon_batch, data, mu, logvar)
+#             loss.backward()
+#             train_loss += loss.item()
+#             self.optimizer.step()
+#             if batch_idx % self.args.log_interval == 0:
+#                 print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+#                     epoch, batch_idx * len(data), len(self.train_loader.dataset),
+#                     100. * batch_idx / len(self.train_loader),
+#                     loss.item() / len(data)))
+
+#         print('====> Epoch: {} Average loss: {:.4f}'.format(
+#               epoch, train_loss / len(self.train_loader.dataset)))
+
+#     def test(self, epoch):
+#         self.model.eval()
+#         test_loss = 0
+#         with torch.no_grad():
+#             for i, (data, _) in enumerate(self.train_loader):
+#                 data = data.to(self.device)
+#                 recon_batch, mu, logvar = self.model(data)
+#                 test_loss += self.loss_function(recon_batch, data, mu, logvar).item()
+
+#         test_loss /= len(self.train_loader.dataset)
+#         print('====> Test set loss: {:.4f}'.format(test_loss))
+
+'''
+ds eurosat
+
+'''
+
 import torch
 import torch.utils.data
 from torch import nn, optim
@@ -123,15 +210,12 @@ from datasets import get_dataloader
 class Network(nn.Module):
     def __init__(self, args):
         super(Network, self).__init__()
-        output_size = 512
-        self.encoder = CNN_Encoder(output_size)
-        self.var = nn.Linear(output_size, args.embedding_size)
-        self.mu = nn.Linear(output_size, args.embedding_size)
+        self.encoder = CNN_Encoder(args.embedding_size)
         self.decoder = CNN_Decoder(args.embedding_size)
 
     def encode(self, x):
-        x = self.encoder(x)
-        return self.mu(x), self.var(x)
+        mu, logvar = self.encoder(x)
+        return mu, logvar
 
     def reparameterize(self, mu, logvar):
         std = torch.exp(0.5 * logvar)
@@ -150,13 +234,16 @@ class VAE(object):
     def __init__(self, args):
         self.args = args
         self.device = torch.device("cuda" if args.cuda else "cpu")
-        self.train_loader = get_dataloader(self.args.dataset, self.args.batch_size)
+        self.train_loader = get_dataloader(self.args.dataset, self.args.batch_size, train=True)
+        self.test_loader = get_dataloader(self.args.dataset, self.args.batch_size, train=False)
 
         self.model = Network(args).to(self.device)
         self.optimizer = optim.Adam(self.model.parameters(), lr=1e-3)
 
     def loss_function(self, recon_x, x, mu, logvar):
+        # Reconstruction loss (assuming input data is normalized to [0, 1])
         BCE = F.binary_cross_entropy(recon_x, x, reduction='sum')
+        # KL divergence
         KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
         return BCE + KLD
 
@@ -171,24 +258,24 @@ class VAE(object):
             loss.backward()
             train_loss += loss.item()
             self.optimizer.step()
+
             if batch_idx % self.args.log_interval == 0:
                 print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                     epoch, batch_idx * len(data), len(self.train_loader.dataset),
                     100. * batch_idx / len(self.train_loader),
                     loss.item() / len(data)))
 
-        print('====> Epoch: {} Average loss: {:.4f}'.format(
-              epoch, train_loss / len(self.train_loader.dataset)))
+        avg_loss = train_loss / len(self.train_loader.dataset)
+        print('====> Epoch: {} Average loss: {:.4f}'.format(epoch, avg_loss))
 
     def test(self, epoch):
         self.model.eval()
         test_loss = 0
         with torch.no_grad():
-            for i, (data, _) in enumerate(self.train_loader):
+            for data, _ in self.test_loader:
                 data = data.to(self.device)
                 recon_batch, mu, logvar = self.model(data)
                 test_loss += self.loss_function(recon_batch, data, mu, logvar).item()
 
-        test_loss /= len(self.train_loader.dataset)
-        print('====> Test set loss: {:.4f}'.format(test_loss))
-
+        test_loss /= len(self.test_loader.dataset)
+        print('====> Test set loss: {:.4f}'.format(test_loss))            
