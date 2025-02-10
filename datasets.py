f56d179430075ce06abbfc03c584a408eca38c37
arpan4713 <arpan.linkd@gmail.com>
2025-02-10 22:38:59 +0530

    correction

--- datasets.py
+++ datasets.py
@@ -46,63 +46,107 @@ EUROSAT MODIFIED
 
 '''
 
+# import torch
+# from torchvision import datasets, transforms
+
+# class MNIST(object):
+#     def __init__(self, args):
+#         kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
+#         self.train_loader = torch.utils.data.DataLoader(
+#             datasets.MNIST('data/mnist', train=True, download=True,
+#                            transform=transforms.ToTensor()),
+#             batch_size=args.batch_size, shuffle=True, **kwargs)
+#         self.test_loader = torch.utils.data.DataLoader(
+#             datasets.MNIST('data/mnist', train=False, transform=transforms.ToTensor()),
+#             batch_size=args.batch_size, shuffle=True, **kwargs)
+
+# class EMNIST(object):
+#     def __init__(self, args):
+#         kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
+#         self.train_loader = torch.utils.data.DataLoader(
+#             datasets.EMNIST('data/emnist', train=True, download=True, split='byclass',
+#                            transform=transforms.ToTensor()),
+#             batch_size=args.batch_size, shuffle=True, **kwargs)
+#         self.test_loader = torch.utils.data.DataLoader(
+#             datasets.EMNIST('data/emnist', train=False, split='byclass',
+#             transform=transforms.ToTensor()),
+#             batch_size=args.batch_size, shuffle=True, **kwargs)
+
+# class FashionMNIST(object):
+#     def __init__(self, args):
+#         kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
+#         self.train_loader = torch.utils.data.DataLoader(
+#             datasets.FashionMNIST('data/fmnist', train=True, download=True,
+#                            transform=transforms.ToTensor()),
+#             batch_size=args.batch_size, shuffle=True, **kwargs)
+#         self.test_loader = torch.utils.data.DataLoader(
+#             datasets.FashionMNIST('data/fmnist', train=False, transform=transforms.ToTensor()),
+#             batch_size=args.batch_size, shuffle=True, **kwargs)
+
+# class EuroSAT(object):
+#     def __init__(self, args):
+#         kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
+
+#         # Define transformations for EuroSAT
+#         transform = transforms.Compose([
+#             transforms.Resize((64, 64)),  # Resize images to 64x64
+#             transforms.ToTensor(),       # Convert to tensor
+#             transforms.Normalize(        # Normalize with ImageNet stats
+#                 mean=[0.485, 0.456, 0.406],  # RGB mean
+#                 std=[0.229, 0.224, 0.225]    # RGB std
+#             )
+#         ])
+
+#         # Load EuroSAT dataset
+#         self.train_loader = torch.utils.data.DataLoader(
+#             datasets.EuroSAT(root='data/eurosat', train=True, download=True, transform=transform),
+#             batch_size=args.batch_size, shuffle=True, **kwargs
+#         )
+#         self.test_loader = torch.utils.data.DataLoader(
+#             datasets.EuroSAT(root='data/eurosat', train=False, download=True, transform=transform),
+#             batch_size=args.batch_size, shuffle=True, **kwargs
+#         )
+
+
+
+
+
+'''
+cp eurosat
+'''
+
+
 import torch
+from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
+from torchvision.datasets import ImageFolder
+import os
 
-class MNIST(object):
-    def __init__(self, args):
-        kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
-        self.train_loader = torch.utils.data.DataLoader(
-            datasets.MNIST('data/mnist', train=True, download=True,
-                           transform=transforms.ToTensor()),
-            batch_size=args.batch_size, shuffle=True, **kwargs)
-        self.test_loader = torch.utils.data.DataLoader(
-            datasets.MNIST('data/mnist', train=False, transform=transforms.ToTensor()),
-            batch_size=args.batch_size, shuffle=True, **kwargs)
+def get_dataloader(dataset_name, batch_size, num_workers=4):
+    """
+    Returns a DataLoader for the specified dataset.
+    """
+    transform = transforms.Compose([
+        transforms.Resize((64, 64)),  # Ensure images are 64x64
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # Standard normalization
+    ])
+    
+    if dataset_name == 'MNIST':
+        dataset = datasets.MNIST(root="data", train=True, download=True, transform=transform)
+    elif dataset_name == 'EMNIST':
+        dataset = datasets.EMNIST(root="data", split='letters', train=True, download=True, transform=transform)
+    elif dataset_name == 'FashionMNIST':
+        dataset = datasets.FashionMNIST(root="data", train=True, download=True, transform=transform)
+    elif dataset_name == 'EuroSAT':
+        dataset_path = "data/EuroSAT"
+        if not os.path.exists(dataset_path):
+            print("Downloading EuroSAT dataset...")
+            dataset = datasets.EuroSAT(root="data", download=True, transform=transform)
+        else:
+            dataset = ImageFolder(root=dataset_path, transform=transform)
+    else:
+        raise ValueError("Unsupported dataset: {}".format(dataset_name))
 
-class EMNIST(object):
-    def __init__(self, args):
-        kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
-        self.train_loader = torch.utils.data.DataLoader(
-            datasets.EMNIST('data/emnist', train=True, download=True, split='byclass',
-                           transform=transforms.ToTensor()),
-            batch_size=args.batch_size, shuffle=True, **kwargs)
-        self.test_loader = torch.utils.data.DataLoader(
-            datasets.EMNIST('data/emnist', train=False, split='byclass',
-            transform=transforms.ToTensor()),
-            batch_size=args.batch_size, shuffle=True, **kwargs)
-
-class FashionMNIST(object):
-    def __init__(self, args):
-        kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
-        self.train_loader = torch.utils.data.DataLoader(
-            datasets.FashionMNIST('data/fmnist', train=True, download=True,
-                           transform=transforms.ToTensor()),
-            batch_size=args.batch_size, shuffle=True, **kwargs)
-        self.test_loader = torch.utils.data.DataLoader(
-            datasets.FashionMNIST('data/fmnist', train=False, transform=transforms.ToTensor()),
-            batch_size=args.batch_size, shuffle=True, **kwargs)
-
-class EuroSAT(object):
-    def __init__(self, args):
-        kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
-
-        # Define transformations for EuroSAT
-        transform = transforms.Compose([
-            transforms.Resize((64, 64)),  # Resize images to 64x64
-            transforms.ToTensor(),       # Convert to tensor
-            transforms.Normalize(        # Normalize with ImageNet stats
-                mean=[0.485, 0.456, 0.406],  # RGB mean
-                std=[0.229, 0.224, 0.225]    # RGB std
-            )
-        ])
-
-        # Load EuroSAT dataset
-        self.train_loader = torch.utils.data.DataLoader(
-            datasets.EuroSAT(root='data/eurosat', train=True, download=True, transform=transform),
-            batch_size=args.batch_size, shuffle=True, **kwargs
-        )
-        self.test_loader = torch.utils.data.DataLoader(
-            datasets.EuroSAT(root='data/eurosat', train=False, download=True, transform=transform),
-            batch_size=args.batch_size, shuffle=True, **kwargs
-        )
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    return dataloader
--- train.py
+++ train.py
@@ -89,9 +89,119 @@ original is aboveeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
 
 '''
 
-EUROSAT EXP
+EUROSAT EXP ds
 
 '''
+
+# import argparse
+# import os
+# import sys
+# import numpy as np
+# import imageio
+# from scipy import ndimage
+
+# import torch
+# from torchvision.utils import save_image
+
+# from models.VAE import VAE
+# from models.AE import AE
+
+# from utils import get_interpolations
+
+# parser = argparse.ArgumentParser(
+#     description='Main function to call training for different AutoEncoders')
+# parser.add_argument('--batch-size', type=int, default=128, metavar='N',
+#                     help='input batch size for training (default: 128)')
+# parser.add_argument('--epochs', type=int, default=10, metavar='N',
+#                     help='number of epochs to train (default: 10)')
+# parser.add_argument('--no-cuda', action='store_true', default=False,
+#                     help='enables CUDA training')
+# parser.add_argument('--seed', type=int, default=42, metavar='S',
+#                     help='random seed (default: 1)')
+# parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+#                     help='how many batches to wait before logging training status')
+# parser.add_argument('--embedding-size', type=int, default=32, metavar='N',
+#                     help='how many batches to wait before logging training status')
+# parser.add_argument('--results_path', type=str, default='results/', metavar='N',
+#                     help='Where to store images')
+# parser.add_argument('--model', type=str, default='AE', metavar='N',
+#                     help='Which architecture to use')
+# parser.add_argument('--dataset', type=str, default='EuroSAT', metavar='N',  # Default dataset set to EuroSAT
+#                     help='Which dataset to use (MNIST, EMNIST, FashionMNIST, EuroSAT)')
+
+# args = parser.parse_args()
+# args.cuda = not args.no_cuda and torch.cuda.is_available()
+# torch.manual_seed(args.seed)
+
+# vae = VAE(args)
+# ae = AE(args)
+# architectures = {'AE': ae,
+#                  'VAE': vae}
+
+# print(args.model)
+# if __name__ == "__main__":
+#     try:
+#         os.stat(args.results_path)
+#     except:
+#         os.mkdir(args.results_path)
+
+#     try:
+#         autoenc = architectures[args.model]
+#     except KeyError:
+#         print('---------------------------------------------------------')
+#         print('Model architecture not supported. ', end='')
+#         print('Maybe you can implement it?')
+#         print('---------------------------------------------------------')
+#         sys.exit()
+
+#     try:
+#         for epoch in range(1, args.epochs + 1):
+#             autoenc.train(epoch)
+#             autoenc.test(epoch)
+#     except (KeyboardInterrupt, SystemExit):
+#         print("Manual Interruption")
+
+#     with torch.no_grad():
+#         images, _ = next(iter(autoenc.test_loader))
+#         images = images.to(autoenc.device)
+#         images_per_row = 16
+#         interpolations = get_interpolations(args, autoenc.model, autoenc.device, images, images_per_row)
+
+#         sample = torch.randn(64, args.embedding_size).to(autoenc.device)
+#         sample = autoenc.model.decode(sample).cpu()
+
+#         # Adjust image dimensions for EuroSAT (64x64 RGB images)
+#         if args.dataset == 'EuroSAT':
+#             save_image(sample.view(64, 3, 64, 64),  # 3 channels, 64x64 resolution
+#                        '{}/sample_{}_{}.png'.format(args.results_path, args.model, args.dataset))
+#             save_image(interpolations.view(-1, 3, 64, 64),
+#                        '{}/interpolations_{}_{}.png'.format(args.results_path, args.model, args.dataset), nrow=images_per_row)
+#         else:
+#             save_image(sample.view(64, 1, 28, 28),
+#                        '{}/sample_{}_{}.png'.format(args.results_path, args.model, args.dataset))
+#             save_image(interpolations.view(-1, 1, 28, 28),
+#                        '{}/interpolations_{}_{}.png'.format(args.results_path, args.model, args.dataset), nrow=images_per_row)
+
+#         # Create animation
+#         interpolations = interpolations.cpu()
+#         if args.dataset == 'EuroSAT':
+#              interpolations = np.reshape(interpolations.data.numpy(), (-1, 64, 64, 3))  # Reshape for RGB
+# else:
+#     interpolations = np.reshape(interpolations.data.numpy(), (-1, 28, 28))
+
+# interpolations = ndimage.zoom(interpolations, (1, 5, 5, 1) if args.dataset == 'EuroSAT' else 5, order=1)
+# interpolations *= 256
+
+# imageio.mimsave('{}/animation_{}_{}.gif'.format(args.results_path, args.model, args.dataset), interpolations.astype(np.uint8))
+
+       
+
+'''
+eurosat cp
+'''
+
+
+
 
 import argparse
 import os
@@ -161,39 +271,37 @@ if __name__ == "__main__":
     except (KeyboardInterrupt, SystemExit):
         print("Manual Interruption")
 
-    with torch.no_grad():
-        images, _ = next(iter(autoenc.test_loader))
-        images = images.to(autoenc.device)
-        images_per_row = 16
-        interpolations = get_interpolations(args, autoenc.model, autoenc.device, images, images_per_row)
-
-        sample = torch.randn(64, args.embedding_size).to(autoenc.device)
-        sample = autoenc.model.decode(sample).cpu()
-
-        # Adjust image dimensions for EuroSAT (64x64 RGB images)
-        if args.dataset == 'EuroSAT':
-            save_image(sample.view(64, 3, 64, 64),  # 3 channels, 64x64 resolution
-                       '{}/sample_{}_{}.png'.format(args.results_path, args.model, args.dataset))
-            save_image(interpolations.view(-1, 3, 64, 64),
-                       '{}/interpolations_{}_{}.png'.format(args.results_path, args.model, args.dataset), nrow=images_per_row)
-        else:
-            save_image(sample.view(64, 1, 28, 28),
-                       '{}/sample_{}_{}.png'.format(args.results_path, args.model, args.dataset))
-            save_image(interpolations.view(-1, 1, 28, 28),
-                       '{}/interpolations_{}_{}.png'.format(args.results_path, args.model, args.dataset), nrow=images_per_row)
-
-        # Create animation
-        interpolations = interpolations.cpu()
-        if args.dataset == 'EuroSAT':
-             interpolations = np.reshape(interpolations.data.numpy(), (-1, 64, 64, 3))  # Reshape for RGB
-else:
-    interpolations = np.reshape(interpolations.data.numpy(), (-1, 28, 28))
-
-interpolations = ndimage.zoom(interpolations, (1, 5, 5, 1) if args.dataset == 'EuroSAT' else 5, order=1)
-interpolations *= 256
-
-imageio.mimsave('{}/animation_{}_{}.gif'.format(args.results_path, args.model, args.dataset), interpolations.astype(np.uint8))
-
-       
 
 
+
+# Ensure correct indentation in conditional statements
+with torch.no_grad():
+    images, _ = next(iter(autoenc.test_loader))
+    images = images.to(autoenc.device)
+    images_per_row = 16
+    interpolations = get_interpolations(args, autoenc.model, autoenc.device, images, images_per_row)
+
+    sample = torch.randn(64, args.embedding_size).to(autoenc.device)
+    sample = autoenc.model.decode(sample).cpu()
+
+    # Adjust image dimensions for EuroSAT (64x64 RGB images)
+    if args.dataset == 'EuroSAT':
+        save_image(sample.view(64, 3, 64, 64),  # 3 channels, 64x64 resolution
+                   '{}/sample_{}_{}.png'.format(args.results_path, args.model, args.dataset))
+        save_image(interpolations.view(-1, 3, 64, 64),
+                   '{}/interpolations_{}_{}.png'.format(args.results_path, args.model, args.dataset), nrow=images_per_row)
+        interpolations = np.reshape(interpolations.data.numpy(), (-1, 64, 64, 3))  # Reshape for RGB
+    else:
+        save_image(sample.view(64, 1, 28, 28),
+                   '{}/sample_{}_{}.png'.format(args.results_path, args.model, args.dataset))
+        save_image(interpolations.view(-1, 1, 28, 28),
+                   '{}/interpolations_{}_{}.png'.format(args.results_path, args.model, args.dataset), nrow=images_per_row)
+        interpolations = np.reshape(interpolations.data.numpy(), (-1, 28, 28))
+
+    # Proper zoom operation for animations
+    zoom_factor = (1, 5, 5, 1) if args.dataset == 'EuroSAT' else (5, 5)
+    interpolations = ndimage.zoom(interpolations, zoom_factor, order=1)
+    interpolations *= 256
+
+    imageio.mimsave('{}/animation_{}_{}.gif'.format(args.results_path, args.model, args.dataset),
+                    interpolations.astype(np.uint8))
