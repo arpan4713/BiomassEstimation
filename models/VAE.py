055cde6f89f3fc8680981312f0ffe907dcec1a9a
arpan4713 <arpan.linkd@gmail.com>
2025-02-12 23:11:57 +0530

    modified

--- models/VAE.py
+++ models/VAE.py
@@ -196,6 +196,100 @@ cp eurosat
 ds eurosat
 
 '''
+# import torch
+# import torch.utils.data
+# from torch import nn, optim
+# from torch.nn import functional as F
+
+# import sys
+# sys.path.append('../')
+# from architectures import FC_Encoder, FC_Decoder, CNN_Encoder, CNN_Decoder
+# from datasets import get_dataloader
+
+# class Network(nn.Module):
+#     def __init__(self, args):
+#         super(Network, self).__init__()
+#         self.encoder = CNN_Encoder(args.embedding_size)
+#         self.decoder = CNN_Decoder(args.embedding_size)
+
+#     def encode(self, x):
+#         mu, logvar = self.encoder(x)
+#         return mu, logvar
+
+#     def reparameterize(self, mu, logvar):
+#         std = torch.exp(0.5 * logvar)
+#         eps = torch.randn_like(std)
+#         return eps.mul(std).add_(mu)
+
+#     def decode(self, z):
+#         return self.decoder(z)
+
+#     def forward(self, x):
+#         mu, logvar = self.encode(x)
+#         z = self.reparameterize(mu, logvar)
+#         return self.decode(z), mu, logvar
+
+# class VAE(object):
+#     def __init__(self, args):
+#         self.args = args
+#         self.device = torch.device("cuda" if args.cuda else "cpu")
+        
+#         # Load data
+#         self.train_loader = get_dataloader(self.args.dataset, self.args.batch_size, train=True)
+#         self.test_loader = get_dataloader(self.args.dataset, self.args.batch_size, train=False)
+
+#         # Initialize model and optimizer
+#         self.model = Network(args).to(self.device)
+#         self.optimizer = optim.Adam(self.model.parameters(), lr=1e-3)
+
+#     def loss_function(self, recon_x, x, mu, logvar):
+#         # Reconstruction loss (Binary Cross-Entropy for normalized data)
+#         BCE = F.binary_cross_entropy(recon_x, x, reduction='sum')
+        
+#         # KL divergence
+#         KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+        
+#         # Total loss
+#         return BCE + KLD
+
+#     def train(self, epoch):
+#         self.model.train()
+#         train_loss = 0
+#         for batch_idx, (data, _) in enumerate(self.train_loader):
+#             data = data.to(self.device)  # Data is already normalized in get_dataloader
+#             self.optimizer.zero_grad()
+#             recon_batch, mu, logvar = self.model(data)
+#             loss = self.loss_function(recon_batch, data, mu, logvar)
+#             loss.backward()
+#             train_loss += loss.item()
+#             self.optimizer.step()
+
+#             if batch_idx % self.args.log_interval == 0:
+#                 print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+#                     epoch, batch_idx * len(data), len(self.train_loader.dataset),
+#                     100. * batch_idx / len(self.train_loader),
+#                     loss.item() / len(data)))
+
+#         avg_loss = train_loss / len(self.train_loader.dataset)
+#         print('====> Epoch: {} Average loss: {:.4f}'.format(epoch, avg_loss))
+
+#     def test(self, epoch):
+#         self.model.eval()
+#         test_loss = 0
+#         with torch.no_grad():
+#             for data, _ in self.test_loader:
+#                 data = data.to(self.device)  # Data is already normalized in get_dataloader
+#                 recon_batch, mu, logvar = self.model(data)
+#                 test_loss += self.loss_function(recon_batch, data, mu, logvar).item()
+
+#         test_loss /= len(self.test_loader.dataset)
+#         print('====> Test set loss: {:.4f}'.format(test_loss))
+
+'''
+new eurosat cp
+
+'''
+
 import torch
 import torch.utils.data
 from torch import nn, optim
@@ -219,7 +313,7 @@ class Network(nn.Module):
     def reparameterize(self, mu, logvar):
         std = torch.exp(0.5 * logvar)
         eps = torch.randn_like(std)
-        return eps.mul(std).add_(mu)
+        return eps * std + mu
 
     def decode(self, z):
         return self.decoder(z)
@@ -240,23 +334,25 @@ class VAE(object):
 
         # Initialize model and optimizer
         self.model = Network(args).to(self.device)
-        self.optimizer = optim.Adam(self.model.parameters(), lr=1e-3)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=1e-4)  # Reduced LR for stability
 
     def loss_function(self, recon_x, x, mu, logvar):
-        # Reconstruction loss (Binary Cross-Entropy for normalized data)
-        BCE = F.binary_cross_entropy(recon_x, x, reduction='sum')
+        # Use MSE loss for continuous data
+        MSE = F.mse_loss(recon_x, x, reduction='sum')
         
         # KL divergence
         KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
         
-        # Total loss
-        return BCE + KLD
+        # KL annealing for stability
+        beta = min(1, self.args.epoch / 10)  # Slowly increase weight over epochs
+        
+        return MSE + beta * KLD
 
     def train(self, epoch):
         self.model.train()
         train_loss = 0
         for batch_idx, (data, _) in enumerate(self.train_loader):
-            data = data.to(self.device)  # Data is already normalized in get_dataloader
+            data = data.to(self.device)
             self.optimizer.zero_grad()
             recon_batch, mu, logvar = self.model(data)
             loss = self.loss_function(recon_batch, data, mu, logvar)
@@ -265,22 +361,20 @@ class VAE(object):
             self.optimizer.step()
 
             if batch_idx % self.args.log_interval == 0:
-                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                    epoch, batch_idx * len(data), len(self.train_loader.dataset),
-                    100. * batch_idx / len(self.train_loader),
-                    loss.item() / len(data)))
+                print(f'Train Epoch: {epoch} [{batch_idx * len(data)}/{len(self.train_loader.dataset)}'
+                      f' ({100. * batch_idx / len(self.train_loader):.0f}%)]	Loss: {loss.item() / len(data):.6f}')
 
         avg_loss = train_loss / len(self.train_loader.dataset)
-        print('====> Epoch: {} Average loss: {:.4f}'.format(epoch, avg_loss))
+        print(f'====> Epoch: {epoch} Average loss: {avg_loss:.4f}')
 
     def test(self, epoch):
         self.model.eval()
         test_loss = 0
         with torch.no_grad():
             for data, _ in self.test_loader:
-                data = data.to(self.device)  # Data is already normalized in get_dataloader
+                data = data.to(self.device)
                 recon_batch, mu, logvar = self.model(data)
                 test_loss += self.loss_function(recon_batch, data, mu, logvar).item()
 
         test_loss /= len(self.test_loader.dataset)
-        print('====> Test set loss: {:.4f}'.format(test_loss))
+        print(f'====> Test set loss: {test_loss:.4f}')
--- train.py
+++ train.py
@@ -203,105 +203,223 @@ eurosat cp
 
 
 
-import argparse
-import os
-import sys
-import numpy as np
-import imageio
-from scipy import ndimage
+# import argparse
+# import os
+# import sys
+# import numpy as np
+# import imageio
+# from scipy import ndimage
+
+# import torch
+# from torchvision.utils import save_image
+
+# from models.VAE import VAE
+# from models.AE import AE
+
+# from utils import get_interpolations
+
+# parser = argparse.ArgumentParser(
+#     description='Main function to call training for different AutoEncoders')
+# parser.add_argument('--batch-size', type=int, default=128, metavar='N',
+#                     help='input batch size for training (default: 128)')
+# parser.add_argument('--epochs', type=int, default=10, metavar='N',
+#                     help='number of epochs to train (default: 10)')
+# parser.add_argument('--no-cuda', action='store_true', default=False,
+#                     help='enables CUDA training')
+# parser.add_argument('--seed', type=int, default=42, metavar='S',
+#                     help='random seed (default: 1)')
+# parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+#                     help='how many batches to wait before logging training status')
+# parser.add_argument('--embedding-size', type=int, default=32, metavar='N',
+#                     help='how many batches to wait before logging training status')
+# parser.add_argument('--results_path', type=str, default='results/', metavar='N',
+#                     help='Where to store images')
+# parser.add_argument('--model', type=str, default='AE', metavar='N',
+#                     help='Which architecture to use')
+# parser.add_argument('--dataset', type=str, default='EuroSAT', metavar='N',  # Default dataset set to EuroSAT
+#                     help='Which dataset to use (MNIST, EMNIST, FashionMNIST, EuroSAT)')
+
+# args = parser.parse_args()
+# args.cuda = not args.no_cuda and torch.cuda.is_available()
+# torch.manual_seed(args.seed)
+
+# vae = VAE(args)
+# ae = AE(args)
+# architectures = {'AE': ae,
+#                  'VAE': vae}
+
+# print(args.model)
+# if __name__ == "__main__":
+#     try:
+#         os.stat(args.results_path)
+#     except:
+#         os.mkdir(args.results_path)
+
+#     try:
+#         autoenc = architectures[args.model]
+#     except KeyError:
+#         print('---------------------------------------------------------')
+#         print('Model architecture not supported. ', end='')
+#         print('Maybe you can implement it?')
+#         print('---------------------------------------------------------')
+#         sys.exit()
+
+#     try:
+#         for epoch in range(1, args.epochs + 1):
+#             autoenc.train(epoch)
+#             autoenc.test(epoch)
+#     except (KeyboardInterrupt, SystemExit):
+#         print("Manual Interruption")
+
+
+
+
+# # Ensure correct indentation in conditional statements
+# with torch.no_grad():
+#     images, _ = next(iter(autoenc.test_loader))
+#     images = images.to(autoenc.device)
+#     images_per_row = 16
+#     interpolations = get_interpolations(args, autoenc.model, autoenc.device, images, images_per_row)
+
+#     sample = torch.randn(64, args.embedding_size).to(autoenc.device)
+#     sample = autoenc.model.decode(sample).cpu()
+
+#     # Adjust image dimensions for EuroSAT (64x64 RGB images)
+#     if args.dataset == 'EuroSAT':
+#         save_image(sample.view(64, 3, 64, 64),  # 3 channels, 64x64 resolution
+#                    '{}/sample_{}_{}.png'.format(args.results_path, args.model, args.dataset))
+#         save_image(interpolations.view(-1, 3, 64, 64),
+#                    '{}/interpolations_{}_{}.png'.format(args.results_path, args.model, args.dataset), nrow=images_per_row)
+#         interpolations = np.reshape(interpolations.data.numpy(), (-1, 64, 64, 3))  # Reshape for RGB
+#     else:
+#         save_image(sample.view(64, 1, 28, 28),
+#                    '{}/sample_{}_{}.png'.format(args.results_path, args.model, args.dataset))
+#         save_image(interpolations.view(-1, 1, 28, 28),
+#                    '{}/interpolations_{}_{}.png'.format(args.results_path, args.model, args.dataset), nrow=images_per_row)
+#         interpolations = np.reshape(interpolations.data.numpy(), (-1, 28, 28))
+
+#     # Proper zoom operation for animations
+#     zoom_factor = (1, 5, 5, 1) if args.dataset == 'EuroSAT' else (5, 5)
+#     interpolations = ndimage.zoom(interpolations, zoom_factor, order=1)
+#     interpolations *= 256
+
+#     imageio.mimsave('{}/animation_{}_{}.gif'.format(args.results_path, args.model, args.dataset),
+#                     interpolations.astype(np.uint8))
+
+
+'''
+eurosat new cp
+
+'''
 
 import torch
-from torchvision.utils import save_image
+from torch import nn
+from torch.nn import functional as F
+from torch.autograd import Variable
+import numpy as np
 
-from models.VAE import VAE
-from models.AE import AE
+def weights_init(m):
+    if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
+        torch.nn.init.xavier_uniform_(m.weight)
+        if m.bias is not None:
+            m.bias.data.fill_(0.01)
 
-from utils import get_interpolations
+class FC_Encoder(nn.Module):
+    def __init__(self, latent_dim, input_size=784):
+        super(FC_Encoder, self).__init__()
+        self.fc1 = nn.Linear(input_size, 512)
+        self.fc_mu = nn.Linear(512, latent_dim)
+        self.fc_logvar = nn.Linear(512, latent_dim)
+        self.apply(weights_init)
 
-parser = argparse.ArgumentParser(
-    description='Main function to call training for different AutoEncoders')
-parser.add_argument('--batch-size', type=int, default=128, metavar='N',
-                    help='input batch size for training (default: 128)')
-parser.add_argument('--epochs', type=int, default=10, metavar='N',
-                    help='number of epochs to train (default: 10)')
-parser.add_argument('--no-cuda', action='store_true', default=False,
-                    help='enables CUDA training')
-parser.add_argument('--seed', type=int, default=42, metavar='S',
-                    help='random seed (default: 1)')
-parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                    help='how many batches to wait before logging training status')
-parser.add_argument('--embedding-size', type=int, default=32, metavar='N',
-                    help='how many batches to wait before logging training status')
-parser.add_argument('--results_path', type=str, default='results/', metavar='N',
-                    help='Where to store images')
-parser.add_argument('--model', type=str, default='AE', metavar='N',
-                    help='Which architecture to use')
-parser.add_argument('--dataset', type=str, default='EuroSAT', metavar='N',  # Default dataset set to EuroSAT
-                    help='Which dataset to use (MNIST, EMNIST, FashionMNIST, EuroSAT)')
+    def forward(self, x):
+        h1 = F.relu(self.fc1(x))
+        mu = self.fc_mu(h1)
+        logvar = self.fc_logvar(h1)
+        return mu, logvar
 
-args = parser.parse_args()
-args.cuda = not args.no_cuda and torch.cuda.is_available()
-torch.manual_seed(args.seed)
+class FC_Decoder(nn.Module):
+    def __init__(self, latent_dim, output_size=784):
+        super(FC_Decoder, self).__init__()
+        self.fc2 = nn.Linear(latent_dim, 512)
+        self.fc3 = nn.Linear(512, output_size)
+        self.apply(weights_init)
 
-vae = VAE(args)
-ae = AE(args)
-architectures = {'AE': ae,
-                 'VAE': vae}
+    def forward(self, z):
+        h2 = F.relu(self.fc2(z))
+        return torch.sigmoid(self.fc3(h2))
 
-print(args.model)
-if __name__ == "__main__":
-    try:
-        os.stat(args.results_path)
-    except:
-        os.mkdir(args.results_path)
+class CNN_Encoder(nn.Module):
+    def __init__(self, latent_dim, input_size=(3, 64, 64)):
+        super(CNN_Encoder, self).__init__()
+        self.input_size = input_size
+        self.channel_mult = 16
+        in_channels = input_size[0]
 
-    try:
-        autoenc = architectures[args.model]
-    except KeyError:
-        print('---------------------------------------------------------')
-        print('Model architecture not supported. ', end='')
-        print('Maybe you can implement it?')
-        print('---------------------------------------------------------')
-        sys.exit()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, self.channel_mult, 4, 2, 1),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(self.channel_mult, self.channel_mult*2, 4, 2, 1),
+            nn.BatchNorm2d(self.channel_mult*2),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(self.channel_mult*2, self.channel_mult*4, 4, 2, 1),
+            nn.BatchNorm2d(self.channel_mult*4),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Conv2d(self.channel_mult*4, self.channel_mult*8, 4, 2, 1),
+            nn.BatchNorm2d(self.channel_mult*8),
+            nn.LeakyReLU(0.2, inplace=True)
+        )
 
-    try:
-        for epoch in range(1, args.epochs + 1):
-            autoenc.train(epoch)
-            autoenc.test(epoch)
-    except (KeyboardInterrupt, SystemExit):
-        print("Manual Interruption")
+        self.flat_fts = self.get_flat_fts(self.conv)
+        self.fc_mu = nn.Linear(self.flat_fts, latent_dim)
+        self.fc_logvar = nn.Linear(self.flat_fts, latent_dim)
+        self.apply(weights_init)
 
+    def get_flat_fts(self, fts):
+        with torch.no_grad():
+            f = fts(torch.ones(1, *self.input_size))
+        return int(np.prod(f.size()[1:]))
 
+    def forward(self, x):
+        x = self.conv(x)
+        x = x.view(-1, self.flat_fts)
+        mu = self.fc_mu(x)
+        logvar = self.fc_logvar(x)
+        return mu, logvar
 
+class CNN_Decoder(nn.Module):
+    def __init__(self, latent_dim, output_size=(3, 64, 64)):
+        super(CNN_Decoder, self).__init__()
+        self.output_size = output_size
+        self.channel_mult = 16
+        out_channels = output_size[0]
 
-# Ensure correct indentation in conditional statements
-with torch.no_grad():
-    images, _ = next(iter(autoenc.test_loader))
-    images = images.to(autoenc.device)
-    images_per_row = 16
-    interpolations = get_interpolations(args, autoenc.model, autoenc.device, images, images_per_row)
+        self.fc = nn.Sequential(
+            nn.Linear(latent_dim, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(True)
+        )
 
-    sample = torch.randn(64, args.embedding_size).to(autoenc.device)
-    sample = autoenc.model.decode(sample).cpu()
+        self.deconv = nn.Sequential(
+            nn.ConvTranspose2d(512, self.channel_mult*8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(self.channel_mult*8),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(self.channel_mult*8, self.channel_mult*4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(self.channel_mult*4),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(self.channel_mult*4, self.channel_mult*2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(self.channel_mult*2),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(self.channel_mult*2, self.channel_mult, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(self.channel_mult),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(self.channel_mult, out_channels, 4, 2, 1, bias=False),
+            nn.Sigmoid()
+        )
+        self.apply(weights_init)
 
-    # Adjust image dimensions for EuroSAT (64x64 RGB images)
-    if args.dataset == 'EuroSAT':
-        save_image(sample.view(64, 3, 64, 64),  # 3 channels, 64x64 resolution
-                   '{}/sample_{}_{}.png'.format(args.results_path, args.model, args.dataset))
-        save_image(interpolations.view(-1, 3, 64, 64),
-                   '{}/interpolations_{}_{}.png'.format(args.results_path, args.model, args.dataset), nrow=images_per_row)
-        interpolations = np.reshape(interpolations.data.numpy(), (-1, 64, 64, 3))  # Reshape for RGB
-    else:
-        save_image(sample.view(64, 1, 28, 28),
-                   '{}/sample_{}_{}.png'.format(args.results_path, args.model, args.dataset))
-        save_image(interpolations.view(-1, 1, 28, 28),
-                   '{}/interpolations_{}_{}.png'.format(args.results_path, args.model, args.dataset), nrow=images_per_row)
-        interpolations = np.reshape(interpolations.data.numpy(), (-1, 28, 28))
-
-    # Proper zoom operation for animations
-    zoom_factor = (1, 5, 5, 1) if args.dataset == 'EuroSAT' else (5, 5)
-    interpolations = ndimage.zoom(interpolations, zoom_factor, order=1)
-    interpolations *= 256
-
-    imageio.mimsave('{}/animation_{}_{}.gif'.format(args.results_path, args.model, args.dataset),
-                    interpolations.astype(np.uint8))
+    def forward(self, x):
+        x = self.fc(x)
+        x = x.view(-1, 512, 1, 1)
+        x = self.deconv(x)
+        return x
