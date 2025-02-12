887c0d5c787d6cf28d5a0a567ce1df1d0957f1ff
arpan4713 <arpan.linkd@gmail.com>
2025-02-12 23:45:15 +0530

    modified

--- train.py
+++ train.py
@@ -203,108 +203,108 @@ eurosat cp
 
 
 
-import argparse
-import os
-import sys
-import numpy as np
-import imageio
-from scipy import ndimage
+# import argparse
+# import os
+# import sys
+# import numpy as np
+# import imageio
+# from scipy import ndimage
 
-import torch
-from torchvision.utils import save_image
+# import torch
+# from torchvision.utils import save_image
 
-from models.VAE import VAE
-from models.AE import AE
+# from models.VAE import VAE
+# from models.AE import AE
 
-from utils import get_interpolations
+# from utils import get_interpolations
 
-parser = argparse.ArgumentParser(
-    description='Main function to call training for different AutoEncoders')
-parser.add_argument('--batch-size', type=int, default=128, metavar='N',
-                    help='input batch size for training (default: 128)')
-parser.add_argument('--epochs', type=int, default=10, metavar='N',
-                    help='number of epochs to train (default: 10)')
-parser.add_argument('--no-cuda', action='store_true', default=False,
-                    help='enables CUDA training')
-parser.add_argument('--seed', type=int, default=42, metavar='S',
-                    help='random seed (default: 1)')
-parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                    help='how many batches to wait before logging training status')
-parser.add_argument('--embedding-size', type=int, default=32, metavar='N',
-                    help='how many batches to wait before logging training status')
-parser.add_argument('--results_path', type=str, default='results/', metavar='N',
-                    help='Where to store images')
-parser.add_argument('--model', type=str, default='AE', metavar='N',
-                    help='Which architecture to use')
-parser.add_argument('--dataset', type=str, default='EuroSAT', metavar='N',  # Default dataset set to EuroSAT
-                    help='Which dataset to use (MNIST, EMNIST, FashionMNIST, EuroSAT)')
+# parser = argparse.ArgumentParser(
+#     description='Main function to call training for different AutoEncoders')
+# parser.add_argument('--batch-size', type=int, default=128, metavar='N',
+#                     help='input batch size for training (default: 128)')
+# parser.add_argument('--epochs', type=int, default=10, metavar='N',
+#                     help='number of epochs to train (default: 10)')
+# parser.add_argument('--no-cuda', action='store_true', default=False,
+#                     help='enables CUDA training')
+# parser.add_argument('--seed', type=int, default=42, metavar='S',
+#                     help='random seed (default: 1)')
+# parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+#                     help='how many batches to wait before logging training status')
+# parser.add_argument('--embedding-size', type=int, default=32, metavar='N',
+#                     help='how many batches to wait before logging training status')
+# parser.add_argument('--results_path', type=str, default='results/', metavar='N',
+#                     help='Where to store images')
+# parser.add_argument('--model', type=str, default='AE', metavar='N',
+#                     help='Which architecture to use')
+# parser.add_argument('--dataset', type=str, default='EuroSAT', metavar='N',  # Default dataset set to EuroSAT
+#                     help='Which dataset to use (MNIST, EMNIST, FashionMNIST, EuroSAT)')
 
-args = parser.parse_args()
-args.cuda = not args.no_cuda and torch.cuda.is_available()
-torch.manual_seed(args.seed)
+# args = parser.parse_args()
+# args.cuda = not args.no_cuda and torch.cuda.is_available()
+# torch.manual_seed(args.seed)
 
-vae = VAE(args)
-ae = AE(args)
-architectures = {'AE': ae,
-                 'VAE': vae}
+# vae = VAE(args)
+# ae = AE(args)
+# architectures = {'AE': ae,
+#                  'VAE': vae}
 
-print(args.model)
-if __name__ == "__main__":
-    try:
-        os.stat(args.results_path)
-    except:
-        os.mkdir(args.results_path)
+# print(args.model)
+# if __name__ == "__main__":
+#     try:
+#         os.stat(args.results_path)
+#     except:
+#         os.mkdir(args.results_path)
 
-    try:
-        autoenc = architectures[args.model]
-    except KeyError:
-        print('---------------------------------------------------------')
-        print('Model architecture not supported. ', end='')
-        print('Maybe you can implement it?')
-        print('---------------------------------------------------------')
-        sys.exit()
+#     try:
+#         autoenc = architectures[args.model]
+#     except KeyError:
+#         print('---------------------------------------------------------')
+#         print('Model architecture not supported. ', end='')
+#         print('Maybe you can implement it?')
+#         print('---------------------------------------------------------')
+#         sys.exit()
 
-    try:
-        for epoch in range(1, args.epochs + 1):
-            autoenc.train(epoch)
-            autoenc.test(epoch)
-    except (KeyboardInterrupt, SystemExit):
-        print("Manual Interruption")
-
-
+#     try:
+#         for epoch in range(1, args.epochs + 1):
+#             autoenc.train(epoch)
+#             autoenc.test(epoch)
+#     except (KeyboardInterrupt, SystemExit):
+#         print("Manual Interruption")
 
 
-# Ensure correct indentation in conditional statements
-with torch.no_grad():
-    images, _ = next(iter(autoenc.test_loader))
-    images = images.to(autoenc.device)
-    images_per_row = 16
-    interpolations = get_interpolations(args, autoenc.model, autoenc.device, images, images_per_row)
 
-    sample = torch.randn(64, args.embedding_size).to(autoenc.device)
-    sample = autoenc.model.decode(sample).cpu()
 
-    # Adjust image dimensions for EuroSAT (64x64 RGB images)
-    if args.dataset == 'EuroSAT':
-        save_image(sample.view(64, 3, 64, 64),  # 3 channels, 64x64 resolution
-                   '{}/sample_{}_{}.png'.format(args.results_path, args.model, args.dataset))
-        save_image(interpolations.view(-1, 3, 64, 64),
-                   '{}/interpolations_{}_{}.png'.format(args.results_path, args.model, args.dataset), nrow=images_per_row)
-        interpolations = np.reshape(interpolations.data.numpy(), (-1, 64, 64, 3))  # Reshape for RGB
-    else:
-        save_image(sample.view(64, 1, 28, 28),
-                   '{}/sample_{}_{}.png'.format(args.results_path, args.model, args.dataset))
-        save_image(interpolations.view(-1, 1, 28, 28),
-                   '{}/interpolations_{}_{}.png'.format(args.results_path, args.model, args.dataset), nrow=images_per_row)
-        interpolations = np.reshape(interpolations.data.numpy(), (-1, 28, 28))
+# # Ensure correct indentation in conditional statements
+# with torch.no_grad():
+#     images, _ = next(iter(autoenc.test_loader))
+#     images = images.to(autoenc.device)
+#     images_per_row = 16
+#     interpolations = get_interpolations(args, autoenc.model, autoenc.device, images, images_per_row)
 
-    # Proper zoom operation for animations
-    zoom_factor = (1, 5, 5, 1) if args.dataset == 'EuroSAT' else (5, 5)
-    interpolations = ndimage.zoom(interpolations, zoom_factor, order=1)
-    interpolations *= 256
+#     sample = torch.randn(64, args.embedding_size).to(autoenc.device)
+#     sample = autoenc.model.decode(sample).cpu()
 
-    imageio.mimsave('{}/animation_{}_{}.gif'.format(args.results_path, args.model, args.dataset),
-                    interpolations.astype(np.uint8))
+#     # Adjust image dimensions for EuroSAT (64x64 RGB images)
+#     if args.dataset == 'EuroSAT':
+#         save_image(sample.view(64, 3, 64, 64),  # 3 channels, 64x64 resolution
+#                    '{}/sample_{}_{}.png'.format(args.results_path, args.model, args.dataset))
+#         save_image(interpolations.view(-1, 3, 64, 64),
+#                    '{}/interpolations_{}_{}.png'.format(args.results_path, args.model, args.dataset), nrow=images_per_row)
+#         interpolations = np.reshape(interpolations.data.numpy(), (-1, 64, 64, 3))  # Reshape for RGB
+#     else:
+#         save_image(sample.view(64, 1, 28, 28),
+#                    '{}/sample_{}_{}.png'.format(args.results_path, args.model, args.dataset))
+#         save_image(interpolations.view(-1, 1, 28, 28),
+#                    '{}/interpolations_{}_{}.png'.format(args.results_path, args.model, args.dataset), nrow=images_per_row)
+#         interpolations = np.reshape(interpolations.data.numpy(), (-1, 28, 28))
+
+#     # Proper zoom operation for animations
+#     zoom_factor = (1, 5, 5, 1) if args.dataset == 'EuroSAT' else (5, 5)
+#     interpolations = ndimage.zoom(interpolations, zoom_factor, order=1)
+#     interpolations *= 256
+
+#     imageio.mimsave('{}/animation_{}_{}.gif'.format(args.results_path, args.model, args.dataset),
+#                     interpolations.astype(np.uint8))
 
 
 '''
@@ -423,3 +423,93 @@ eurosat new cp
 #         x = x.view(-1, 512, 1, 1)
 #         x = self.deconv(x)
 #         return x
+
+
+'''
+eurosat newer cp
+
+'''
+
+import argparse
+import os
+import sys
+import numpy as np
+import imageio
+from scipy import ndimage
+
+import torch
+from torchvision.utils import save_image
+
+from models.VAE import VAE
+from models.AE import AE
+from utils import get_interpolations
+
+parser = argparse.ArgumentParser(
+    description='Main function to call training for different AutoEncoders')
+parser.add_argument('--batch-size', type=int, default=128, metavar='N',
+                    help='input batch size for training (default: 128)')
+parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                    help='number of epochs to train (default: 10)')
+parser.add_argument('--no-cuda', action='store_true', default=False,
+                    help='disables CUDA training')
+parser.add_argument('--seed', type=int, default=42, metavar='S',
+                    help='random seed (default: 42)')
+parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                    help='how many batches to wait before logging training status')
+parser.add_argument('--embedding-size', type=int, default=32, metavar='N',
+                    help='embedding size for latent space')
+parser.add_argument('--results_path', type=str, default='results/', metavar='N',
+                    help='Where to store images')
+parser.add_argument('--model', type=str, default='AE', metavar='N',
+                    help='Which architecture to use')
+parser.add_argument('--dataset', type=str, default='EuroSAT', metavar='N',
+                    help='Which dataset to use (MNIST, EMNIST, FashionMNIST, EuroSAT)')
+
+args = parser.parse_args()
+args.cuda = not args.no_cuda and torch.cuda.is_available()
+torch.manual_seed(args.seed)
+
+torch.device("cuda" if args.cuda else "cpu")
+
+vae = VAE(args)
+ae = AE(args)
+architectures = {'AE': ae, 'VAE': vae}
+
+if __name__ == "__main__":
+    os.makedirs(args.results_path, exist_ok=True)
+    
+    if args.model not in architectures:
+        print(f'Error: Model {args.model} not supported!')
+        sys.exit(1)
+    
+    autoenc = architectures[args.model]
+    
+    try:
+        for epoch in range(1, args.epochs + 1):
+            autoenc.train(epoch)
+            autoenc.test(epoch)
+    except (KeyboardInterrupt, SystemExit):
+        print("Manual Interruption")
+
+    # Perform interpolation and save results
+    with torch.no_grad():
+        images, _ = next(iter(autoenc.test_loader))
+        images = images.to(autoenc.device)
+        images_per_row = 16
+        interpolations = get_interpolations(args, autoenc.model, autoenc.device, images, images_per_row)
+
+        sample = torch.randn(64, args.embedding_size).to(autoenc.device)
+        sample = autoenc.model.decode(sample).cpu()
+
+        if args.dataset == 'EuroSAT':
+            save_image(sample.view(64, 3, 64, 64), f"{args.results_path}/sample_{args.model}_{args.dataset}.png")
+            save_image(interpolations.view(-1, 3, 64, 64), f"{args.results_path}/interpolations_{args.model}_{args.dataset}.png", nrow=images_per_row)
+        else:
+            save_image(sample.view(64, 1, 28, 28), f"{args.results_path}/sample_{args.model}_{args.dataset}.png")
+            save_image(interpolations.view(-1, 1, 28, 28), f"{args.results_path}/interpolations_{args.model}_{args.dataset}.png", nrow=images_per_row)
+
+        interpolations = interpolations.cpu().numpy()
+        zoom_factor = (1, 5, 5, 1) if args.dataset == 'EuroSAT' else (5, 5)
+        interpolations = ndimage.zoom(interpolations, zoom_factor, order=1) * 256
+
+        imageio.mimsave(f"{args.results_path}/animation_{args.model}_{args.dataset}.gif", interpolations.astype(np.uint8))
--- utils.py
+++ utils.py
@@ -1,3 +1,50 @@
+# import numpy as np
+# import torch
+
+# def get_interpolations(args, model, device, images, images_per_row=20):
+#     model.eval()
+#     with torch.no_grad():
+#         def interpolate(t1, t2, num_interps):
+#             alpha = np.linspace(0, 1, num_interps+2)
+#             interps = []
+#             for a in alpha:
+#                 interps.append(a*t2.view(1, -1) + (1 - a)*t1.view(1, -1))
+#             return torch.cat(interps, 0)
+
+#         if args.model == 'VAE':
+#             mu, logvar = model.encode(images.view(-1, 784))
+#             embeddings = model.reparameterize(mu, logvar).cpu()
+#         elif args.model == 'AE':
+#             embeddings = model.encode(images.view(-1, 784))
+            
+#         interps = []
+#         for i in range(0, images_per_row+1, 1):
+#             interp = interpolate(embeddings[i], embeddings[i+1], images_per_row-4)
+#             interp = interp.to(device)
+#             interp_dec = model.decode(interp)
+#             line = torch.cat((images[i].view(-1, 784), interp_dec, images[i+1].view(-1, 784)))
+#             interps.append(line)
+#         # Complete the loop and append the first image again
+#         interp = interpolate(embeddings[i+1], embeddings[0], images_per_row-4)
+#         interp = interp.to(device)
+#         interp_dec = model.decode(interp)
+#         line = torch.cat((images[i+1].view(-1, 784), interp_dec, images[0].view(-1, 784)))
+#         interps.append(line)
+
+#         interps = torch.cat(interps, 0).to(device)
+#     return interps
+
+
+'''
+above is original
+
+'''
+
+
+'''
+below is eurosat cp 
+'''
+
 import numpy as np
 import torch
 
@@ -5,31 +52,33 @@ def get_interpolations(args, model, device, images, images_per_row=20):
     model.eval()
     with torch.no_grad():
         def interpolate(t1, t2, num_interps):
-            alpha = np.linspace(0, 1, num_interps+2)
-            interps = []
-            for a in alpha:
-                interps.append(a*t2.view(1, -1) + (1 - a)*t1.view(1, -1))
-            return torch.cat(interps, 0)
+            alpha = np.linspace(0, 1, num_interps + 2)
+            interps = [(1 - a) * t1 + a * t2 for a in alpha]
+            return torch.stack(interps, 0)
 
+        images = images.to(device)
+        
         if args.model == 'VAE':
-            mu, logvar = model.encode(images.view(-1, 784))
+            mu, logvar = model.encode(images)
             embeddings = model.reparameterize(mu, logvar).cpu()
         elif args.model == 'AE':
-            embeddings = model.encode(images.view(-1, 784))
-            
+            embeddings = model.encode(images).cpu()
+        else:
+            raise ValueError(f"Unsupported model type: {args.model}")
+        
         interps = []
-        for i in range(0, images_per_row+1, 1):
-            interp = interpolate(embeddings[i], embeddings[i+1], images_per_row-4)
-            interp = interp.to(device)
-            interp_dec = model.decode(interp)
-            line = torch.cat((images[i].view(-1, 784), interp_dec, images[i+1].view(-1, 784)))
-            interps.append(line)
-        # Complete the loop and append the first image again
-        interp = interpolate(embeddings[i+1], embeddings[0], images_per_row-4)
-        interp = interp.to(device)
-        interp_dec = model.decode(interp)
-        line = torch.cat((images[i+1].view(-1, 784), interp_dec, images[0].view(-1, 784)))
-        interps.append(line)
+        num_samples = embeddings.size(0)
 
+        for i in range(num_samples - 1):
+            interp = interpolate(embeddings[i], embeddings[i + 1], images_per_row - 4).to(device)
+            interp_dec = model.decode(interp)
+            line = torch.cat([images[i].unsqueeze(0), interp_dec, images[i + 1].unsqueeze(0)])
+            interps.append(line)
+        
+        interp = interpolate(embeddings[-1], embeddings[0], images_per_row - 4).to(device)
+        interp_dec = model.decode(interp)
+        line = torch.cat([images[-1].unsqueeze(0), interp_dec, images[0].unsqueeze(0)])
+        interps.append(line)
+        
         interps = torch.cat(interps, 0).to(device)
     return interps
